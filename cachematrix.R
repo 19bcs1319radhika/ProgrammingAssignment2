5a720fc31f5130e8400e7d4e573f8bf1ff6c9b76
19bcs1319radhika <93004231+19bcs1319radhika@users.noreply.github.com>
2022-04-14 01:43:27 +0530

    Update cachematrix.R
    BY RADHIKA

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+##This code is written by RADHIKA
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## I created x as the input of matrix
+## solving "rads" as a null
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+        rads <- NULL
+        set <-function(am)
+       {
+                x <<- am
+                rads <<- NULL
+       }
+       get <- function() x
+        setsolve <-function(solve) rads <<- solve
+        getsolve<- function() rads
+        list( set= set, get= get, setsolve= setsolve, getsolve = getsolve)
+                
 }
-
-
-## Write a short comment describing this function
-
+## This code is written by RADHIKA
+## Write a short comment "solve" for getting the function
+##Changed "mean" to "solve to get out output
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+        rads <- x$getsolve()
+        if(!is.null(rads))
+        {
+                message ("Getting Inversed Matrix As:")
+                return(rads)
+        }
+        data <-x$get()
+        rads <- solve(data, ...)
+        x$setsolve(rads)
+        rads
 }
